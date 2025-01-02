602ab04742aee80f3075df81a4bee921599cc71a
John Wesley Baki <johnwesleybaki3@gmail.com>
2025-01-02 10:25:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black flex items-center justify-center h-screen w-screen">
+      <h1 className="text-white text-5xl">Coming soon</h1>
+    </div>
+  );
+}
+
+export default App;
