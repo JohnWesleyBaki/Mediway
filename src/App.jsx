c946f0fd4b383e66865972a778e4dd476c494ffc
John Wesley Baki <johnwesleybaki3@gmail.com>
2025-01-02 23:44:52 +0530

    added the logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 function App() {
   return (
     <div className="bg-black flex items-center justify-center h-screen w-screen">
+      <div>
+        <img src="./1.svg" alt="Logo" className="w-32 h-32" />
+        <img src="./2.svg" alt="Logo" className="w-32 h-32" />
+        <img src="./3.svg" alt="Logo" className="w-32 h-32" />
+      </div>
       <h1 className="text-white text-5xl">Coming soon</h1>
     </div>
   );
